05ec4b1bf6ac0b16d1c44a55c698204bc51c89f4
LAPTOP-TE322P17\user <jaewon8752@gmail.com>
2022-06-12 16:03:09 +0900

    first commit

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts', [PostController::class, 'create']);
+Route::get('/posts/{id}', [PostController::class, 'read']);
+Route::patch('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'delete']);
+
+Route::post('/posts/{postId}/comments', [CommentController::class, 'create']);
+Route::delete('/posts/{postId}/comments/{id}', [CommentController::class, 'delete']);
